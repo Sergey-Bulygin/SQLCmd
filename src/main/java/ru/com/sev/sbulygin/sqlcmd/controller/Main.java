5fc9738751009f35da77f9df67f2bbd0a74bf1bf
sergeyBulygin <sergteletrade@gmail.com>
2019-01-31 19:27:21 +0300

    Create new project [#2]

---
+++ src/main/java/ru/com/sev/sbulygin/sqlcmd/controller/Main.java
@@ -0,0 +1,11 @@
+package ru.com.sev.sbulygin.sqlcmd.controller;
+
+/**
+ * Class Main.
+ *
+ * @author anonymous.
+ * @version 1.0.
+ * @since 31.01.19.
+ */
+public class Main {
+}
---
+++ src/main/java/ru/com/sev/sbulygin/sqlcmd/model/DatabaseManager.java
@@ -0,0 +1,11 @@
+package ru.com.sev.sbulygin.sqlcmd.model;
+
+/**
+ * Class DatabaseManager.
+ *
+ * @author anonymous.
+ * @version 1.0.
+ * @since 31.01.19.
+ */
+public class DatabaseManager {
+}
