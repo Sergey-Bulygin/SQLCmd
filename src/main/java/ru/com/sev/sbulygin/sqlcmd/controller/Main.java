42b0978f425287d704276cc5161ac029d8592236
Sergey-Bulygin <alabamka78@gmail.com>
2020-04-08 15:09:59 +0300

    Test connect SQL base #SQLcmd_task_3

--- src/main/java/ru/com/sev/sbulygin/sqlcmd/controller/Main.java
+++ src/main/java/ru/com/sev/sbulygin/sqlcmd/controller/Main.java
@@ -1,5 +1,10 @@
 package ru.com.sev.sbulygin.sqlcmd.controller;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  * Class   Main
  * Created 02/04/2020 - 22:24
@@ -7,4 +12,13 @@ package ru.com.sev.sbulygin.sqlcmd.controller;
  * Author  Sergey Bulygin
  */
 public class Main {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        Class.forName("org.postgresql.Driver");
+        Connection conn = DriverManager.getConnection(
+                "jdbc:postgresql://127.0.0.1:5432/sqlcmd", "postgres", "bbfd50ago"); {
+        }
+        //insert
+        Statement stmt = conn.createStatement();
+        stmt.executeUpdate("INSERT INTO public.users (name, password)" + "VALUES ('Ivan', 'Budko')");
+    }
 }
