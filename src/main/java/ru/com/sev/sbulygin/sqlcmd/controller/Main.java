72e8a45c4b4275c3e7506546809b66bedcba5c86
Sergey-Bulygin <alabamka78@gmail.com>
2020-04-10 22:19:58 +0300

    Create statement select, insert #SQLcmd_task_4

--- src/main/java/ru/com/sev/sbulygin/sqlcmd/controller/Main.java
+++ src/main/java/ru/com/sev/sbulygin/sqlcmd/controller/Main.java
@@ -1,9 +1,6 @@
 package ru.com.sev.sbulygin.sqlcmd.controller;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 /**
  * Class   Main
@@ -14,11 +11,27 @@ import java.sql.Statement;
 public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         Class.forName("org.postgresql.Driver");
-        Connection conn = DriverManager.getConnection(
+        Connection connection = DriverManager.getConnection(
                 "jdbc:postgresql://127.0.0.1:5432/sqlcmd", "postgres", "bbfd50ago"); {
         }
         //insert
-        Statement stmt = conn.createStatement();
-        stmt.executeUpdate("INSERT INTO public.users (name, password)" + "VALUES ('Ivan', 'Budko')");
+        Statement stmt = connection.createStatement();
+        stmt.executeUpdate("INSERT INTO public.users (name, password)" +
+                "VALUES ('Ivan', 'Budko')");
+        stmt.close();
+
+        //select
+        stmt = connection.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT * FROM public.users WHERE id > 5");
+        while (rs.next()) {
+            System.out.println("id:" + rs.getString("id"));
+            System.out.println("name:" + rs.getString("name"));
+            System.out.println("password:" + rs.getString("password"));
+            System.out.println("----------");
+
+        }
+        rs.close();
+        stmt.close();
+        connection.close();
     }
 }
